a6c9d2e1759f07e5d1737e8a19a63f0971bf63fc
ajassi9751 <ajassi9751@gmail.com>
2026-09-02 16:47:06 +0000

    Got connection to work

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use libssh_rs::Session;
 
 fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
-    let session = Session::new()?.sftp()?;
+    let session = Session::new()?;
+    // session.set_option(libssh_rs::SshOption::Hostname(String::from("localhost")))?;
+    session.set_option(libssh_rs::SshOption::Hostname(String::from("test.rebex.net")))?;
+    session.connect()?;
+    // session.userauth_keyboard_interactive(None, None)?;
+    session.userauth_password(Some("demo"), Some("password"))?;
+    let channel = session.new_channel()?;
+    channel.open_session()?;
+    println!("Session opened");
+    channel.request_exec("whoami")?;
+    channel.send_eof()?;
     anyhow::Ok(())
 }
