c8e9124094d27b7a5775985826a00e1e63fcd244
ajassi9751 <ajassi9751@gmail.com>
2026-09-02 16:52:15 +0000

    Got the exit code and stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 use libssh_rs::Session;
 
 fn main() -> anyhow::Result<()> {
@@ -9,8 +11,13 @@ fn main() -> anyhow::Result<()> {
     session.userauth_password(Some("demo"), Some("password"))?;
     let channel = session.new_channel()?;
     channel.open_session()?;
-    println!("Session opened");
+    // println!("Session opened");
     channel.request_exec("whoami")?;
     channel.send_eof()?;
+    let mut stdout = String::new();
+    channel.stdout().read_to_string(&mut stdout)?;
+    let res = channel.get_exit_status().unwrap();
+    print!("stdout: {}", stdout);
+    println!("res: {}", res);
     anyhow::Ok(())
 }
