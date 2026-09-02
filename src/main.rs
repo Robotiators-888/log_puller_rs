2904ff441d5caa62c40778e1484fe67dde6eca4d
Aikam Jassi <ajassi9751@gmail.com>
2026-09-02 18:30:05 -0400

    Can now read and list files

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,15 @@ fn main() -> anyhow::Result<()> {
     session.connect()?;
     // session.userauth_keyboard_interactive(None, None)?;
     session.userauth_password(Some("demo"), Some("password"))?;
+    let sftp = session.sftp()?;
+    let mut dir = sftp.open("readme.txt", libssh_rs::OpenFlags::READ_ONLY, 0)?;
+    let mut file_contents = String::new();
+    dir.read_to_string(&mut file_contents)?;
+    println!("File contents: {}", file_contents);
     let channel = session.new_channel()?;
     channel.open_session()?;
     // println!("Session opened");
-    channel.request_exec("whoami")?;
+    channel.request_exec("ls")?;
     channel.send_eof()?;
     let mut stdout = String::new();
     channel.stdout().read_to_string(&mut stdout)?;
