9ab8378efa2b0d5a1b20e9b864393e96619a7f71
ajassi9751 <ajassi9751@gmail.com>
2026-09-04 22:44:25 -0400

    Removed some bug fixing code

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,8 @@ fn main() -> anyhow::Result<()> {
         "test.rebex.net",
     )))?;
     session.connect()?;
-    // session.userauth_keyboard_interactive(None, None)?;
     session.userauth_password(Some("demo"), Some("password"))?;
     let sftp = session.sftp()?;
-    println!("Entries: {}", sftp.read_dir(".")?.into_iter().len());
     for (i, f) in sftp.read_dir(".")?.into_iter().enumerate() {
         if let libssh_rs::FileType::Directory = f
             .file_type()
@@ -20,15 +18,10 @@ fn main() -> anyhow::Result<()> {
         {
             continue;
         }
-        println!("Ran: {}", i);
         let name = f.name().ok_or(anyhow::anyhow!("Failed to get file name"))?;
-        println!("Got name: {}", name);
         let mut rfile = sftp.open(name, libssh_rs::OpenFlags::READ_ONLY, 0)?;
-        println!("Opened file");
         let mut file = std::fs::File::create(name)?;
-        println!("Created file");
         std::io::copy(&mut rfile, &mut file)?;
-        println!("Copied file");
     }
     anyhow::Ok(())
 }
