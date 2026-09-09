7796202a20b441a741b93e8a59fe5b2cecd12b5b
ajassi9751 <ajassi9751@gmail.com>
2026-09-09 21:16:43 +0000

    Ready for threads

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() -> anyhow::Result<()> {
     println!("{:?}", map);
     // let mut map: Arc<Mutex<HashMap<PathBuf, u64>>> = Arc::new(Mutex::new(map));
     // // Is Box<Path> better again?
-    // let mut filenames: Vec<(PathBuf, PathBuf)> = Vec::new();
+    let mut filesinfos: Vec<(PathBuf, PathBuf, u64)> = Vec::new();
     let session = Session::new()?;
     // session.set_option(libssh_rs::SshOption::Hostname(String::from("localhost")))?;
     session.set_option(libssh_rs::SshOption::Hostname(String::from(
@@ -22,11 +22,12 @@ fn main() -> anyhow::Result<()> {
     session.connect()?;
     session.userauth_password(Some("demo"), Some("password"))?;
     let sftp = session.sftp()?;
-    copy_folder(&sftp, Path::new("."), Path::new("data"), &mut map)?;
+    get_folder_info(&sftp, Path::new("."), Path::new("data"), &mut filesinfos, &mut map)?;
+    filesinfos.into_iter().for_each(move |i| copy_file(&sftp, i, &mut map).unwrap());
     anyhow::Ok(())
 }
 
-fn copy_folder(sftp: &Sftp, dirpath: &Path, prefix: &Path, map: &mut HashMap<PathBuf, u64>) -> anyhow::Result<()> {
+fn get_folder_info(sftp: &Sftp, dirpath: &Path, prefix: &Path, filesinfos: &mut Vec<(PathBuf, PathBuf, u64)>, map: &mut HashMap<PathBuf, u64>) -> anyhow::Result<()> {
     println!("Path: {}", dirpath.to_str().unwrap());
     println!("Prefix: {}", prefix.to_str().unwrap());
     for f in sftp
@@ -49,44 +50,44 @@ fn copy_folder(sftp: &Sftp, dirpath: &Path, prefix: &Path, map: &mut HashMap<Pat
             }
             let prefixdir = &prefix.join(dirname);
             std::fs::create_dir_all(prefixdir)?;
-            copy_folder(sftp, &dirpath.join(dirname), prefixdir, map)?;
+            get_folder_info(sftp, &dirpath.join(dirname), prefixdir, filesinfos, map)?;
         } else if let libssh_rs::FileType::Regular = filetype {
-            let name = f.name().ok_or(anyhow::anyhow!("Failed to get file name"))?;
-            println!("Name: {}", name);
-            let rfilesize = f.len().ok_or(anyhow::anyhow!("Failed to get file size"))?;
-            if let Some(fsize) = map.get(&prefix.join(name)) {
-                if *fsize != rfilesize {
-                    println!("Updated file");
-                    let mut rfile = sftp.open(
-                        &dirpath
-                            .join(name)
-                            .to_str()
-                            .ok_or(anyhow::anyhow!("Failed to join remote file path"))?,
-                        libssh_rs::OpenFlags::READ_ONLY,
-                        0,
-                    )?;
-                    let mut file = std::fs::File::open(&prefix.join(name))?;
-                    std::io::copy(&mut rfile, &mut file)?;
-                }
-            }
-            else {
-                println!("Copied file");
-                let mut rfile = sftp.open(
-                    &dirpath
-                        .join(name)
-                        .to_str()
-                        .ok_or(anyhow::anyhow!("Failed to join remote file path"))?,
-                    libssh_rs::OpenFlags::READ_ONLY,
-                    0,
-                )?;
-                let fname = prefix.join(name);
-                let mut file = std::fs::File::create(&fname)?;
-                std::io::copy(&mut rfile, &mut file)?;
-                map.insert(fname, rfilesize);
-            }
+            let fname = f.name().ok_or(anyhow::anyhow!("Failed to get file name"))?;
+            filesinfos.push((prefix.join(fname), dirpath.join(fname), f.len().ok_or(anyhow::anyhow!("Failed to get file size"))?));
         } else {
             return anyhow::Result::Err(anyhow::anyhow!("Invalid file type encountered"));
         }
+    }
+    anyhow::Ok(())
+}
+
+fn copy_file(sftp: &Sftp, filesinfo: (PathBuf, PathBuf, u64), map: &mut HashMap<PathBuf, u64>) -> anyhow::Result<()> {
+    if let Some(fsize) = map.get(&filesinfo.0) {
+        if *fsize != filesinfo.2 {
+            println!("Updated file: {:?}, {:?}, {:?}", filesinfo.0, filesinfo.1, filesinfo.2);
+            let mut rfile = sftp.open(
+                &filesinfo.1
+                    .to_str()
+                    .ok_or(anyhow::anyhow!("Failed to join remote file path"))?,
+                libssh_rs::OpenFlags::READ_ONLY,
+                0,
+            )?;
+            let mut file = std::fs::File::open(&filesinfo.0)?;
+            std::io::copy(&mut rfile, &mut file)?;
+        }
+    }
+    else {
+        println!("Copied file: {:?}, {:?}, {:?}", filesinfo.0, filesinfo.1, filesinfo.2);
+        let mut rfile = sftp.open(
+            &filesinfo.1
+                .to_str()
+                .ok_or(anyhow::anyhow!("Failed to join remote file path"))?,
+            libssh_rs::OpenFlags::READ_ONLY,
+            0,
+        )?;
+        let mut file = std::fs::File::create(&filesinfo.0)?;
+        std::io::copy(&mut rfile, &mut file)?;
+        map.insert(filesinfo.0, filesinfo.2);
     }
     anyhow::Ok(())
 }
